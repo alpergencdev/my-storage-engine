5dffebae9467ede7825d770bcb741d358bfca50f
Mehmet Alper Genç <45502674+alpergencdev@users.noreply.github.com>
2024-02-05 18:00:11 +0300

    Refactor common custom exception classes

--- src/main/java/com/alpergencdev/common/error/MyStorageEngineException.java
+++ src/main/java/com/alpergencdev/common/error/MyStorageEngineException.java
@@ -1,4 +1,24 @@
 package com.alpergencdev.common.error;
 
 public class MyStorageEngineException extends Exception {
+
+    public MyStorageEngineException() {
+        super();
+    }
+
+    public MyStorageEngineException(String message) {
+        super(message);
+    }
+
+    public MyStorageEngineException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyStorageEngineException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MyStorageEngineException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/com/alpergencdev/common/error/MyStorageEngineRuntimeException.java
+++ src/main/java/com/alpergencdev/common/error/MyStorageEngineRuntimeException.java
@@ -1,4 +1,23 @@
 package com.alpergencdev.common.error;
 
 public class MyStorageEngineRuntimeException extends RuntimeException {
+    public MyStorageEngineRuntimeException() {
+        super();
+    }
+
+    public MyStorageEngineRuntimeException(String message) {
+        super(message);
+    }
+
+    public MyStorageEngineRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyStorageEngineRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MyStorageEngineRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
